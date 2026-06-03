52c3d1f2de1776d664690b0e0beaa26397293f4a
Sophia Rosicky <sophia.rosicky33@gmail.com>
2026-06-03 13:19:22 -0600

    Made a colors file because Dudraw hates me and i always have to do this

---
+++ colors.py
@@ -0,0 +1,21 @@
+import dudraw
+
+# colors!!
+COLOR_MAP = {
+    "white":      dudraw.WHITE,
+    "black":      dudraw.BLACK,
+    "red":        dudraw.RED,
+    "green":      dudraw.GREEN,
+    "blue":       dudraw.BLUE,
+    "cyan":       dudraw.CYAN,
+    "magenta":    dudraw.MAGENTA,
+    "yellow":     dudraw.YELLOW,
+    "orange":     dudraw.ORANGE,
+    "violet":     dudraw.VIOLET,
+    "pink":       dudraw.PINK,
+    "gray":       dudraw.GRAY,
+    "light_gray": dudraw.LIGHT_GRAY,
+    "dark_red":   dudraw.DARK_RED,
+    "dark_green": dudraw.DARK_GREEN,
+    "dark_blue":  dudraw.DARK_BLUE,
+}
